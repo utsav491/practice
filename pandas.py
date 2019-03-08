3d55e08456a72c6f6d5d72ddb3219bb69f28dd1a
utsav491 <utsav491@gmail.com>
2019-03-08 17:54:34 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,5 +1,5 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv()
+df = pd.read_csv("C:\\Users\\utsav\\Desktop\\Python_Data_Sets_Data_Camp\\Merging Data Frames in Pandas\\Sales\\sales-jan-2015.csv" , index_col = "Date" , parse_dates= True))
 print(df.head())
